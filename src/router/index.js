a42b7747d0a287474677eb05902e4d110598d923
Henadzi Arashkevich <oreshkevich85@mail.ru>
2022-10-04 12:00:38 +0300

    feat: auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import Register from '@/views/RegisterView.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomeView,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: '/register',
+    name: 'register',
+    component: Register,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/store/modules/auth.js
@@ -0,0 +1,63 @@
+import authApi from '@/api/auth';
+import {setItem} from '@/helpers/persistanceStorage';
+
+const state = {
+  isSubmitting: false,
+  isLoggedIn: null,
+  currentUser: null,
+  validationErrors: null,
+};
+
+export const mutationTypes = {
+  registerStart: '[auth] Register start',
+  registerSuccess: '[auth] Register success',
+  registerFailure: '[auth] Register failure',
+};
+
+export const actionTypes = {
+  register: '[auth] Register',
+};
+
+const mutations = {
+  [mutationTypes.registerStart](state) {
+    state.isSubmitting = true;
+    state.validationErrors = null;
+  },
+  [mutationTypes.registerSuccess](state, payload) {
+    state.isSubmitting = false;
+    state.isLoggedIn = true;
+    state.currentUser = payload;
+  },
+  [mutationTypes.registerFailure](state, payload) {
+    state.isSubmitting = false;
+    state.validationErrors = payload;
+  },
+};
+
+const actions = {
+  [actionTypes.register](context, credentials) {
+    return new Promise((resolve) => {
+      context.commit(mutationTypes.registerStart);
+      authApi
+        .register(credentials)
+        .then((response) => {
+          context.commit(mutationTypes.registerSuccess, response.data.user);
+          setItem('accessToken', response.data.user.token);
+          resolve(response.data.user);
+        })
+        .catch((result) => {
+          context.commit(
+            mutationTypes.registerFailure,
+            result.response.data.errors
+          );
+        });
+    });
+  },
+};
+
+export default {
+  state,
+  actions,
+  mutations,
+  // getters
+};
